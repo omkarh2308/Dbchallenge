32e8bb25862d611b6a50554aac79e8a86a7b9bfa
omkarh2308 <omkar2308@gmail.com>
2020-09-08 11:37:01 +0530

    updated service and controller

--- challenge/src/main/java/com/db/awmd/challenge/web/AccountsController.java
+++ challenge/src/main/java/com/db/awmd/challenge/web/AccountsController.java
@@ -3,6 +3,9 @@ package com.db.awmd.challenge.web;
 import com.db.awmd.challenge.domain.Account;
 import com.db.awmd.challenge.exception.DuplicateAccountIdException;
 import com.db.awmd.challenge.service.AccountsService;
+
+import java.math.BigDecimal;
+
 import javax.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +17,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -21,30 +25,39 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class AccountsController {
 
-  private final AccountsService accountsService;
+	@Autowired
+	private final AccountsService accountsService;
 
-  @Autowired
-  public AccountsController(AccountsService accountsService) {
-    this.accountsService = accountsService;
-  }
+	@Autowired
+	public AccountsController(AccountsService accountsService) {
+		this.accountsService = accountsService;
+	}
 
-  @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
-  public ResponseEntity<Object> createAccount(@RequestBody @Valid Account account) {
-    log.info("Creating account {}", account);
+	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Object> createAccount(@RequestBody @Valid Account account) {
+		log.info("Creating account {}", account);
 
-    try {
-    this.accountsService.createAccount(account);
-    } catch (DuplicateAccountIdException daie) {
-      return new ResponseEntity<>(daie.getMessage(), HttpStatus.BAD_REQUEST);
-    }
+		try {
+			this.accountsService.createAccount(account);
+		} catch (DuplicateAccountIdException daie) {
+			return new ResponseEntity<>(daie.getMessage(), HttpStatus.BAD_REQUEST);
+		}
 
-    return new ResponseEntity<>(HttpStatus.CREATED);
-  }
+		return new ResponseEntity<>(HttpStatus.CREATED);
+	}
 
-  @GetMapping(path = "/{accountId}")
-  public Account getAccount(@PathVariable String accountId) {
-    log.info("Retrieving account for id {}", accountId);
-    return this.accountsService.getAccount(accountId);
-  }
+	@GetMapping(path = "/{accountId}")
+	public Account getAccount(@PathVariable String accountId) {
+		log.info("Retrieving account for id {}", accountId);
+		return this.accountsService.getAccount(accountId);
+	}
+
+	@PostMapping(path = "/transferAmt", consumes = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Object> transactionalDetails(@RequestParam("accountFrom") String accountFrom,
+			@RequestParam("accountTo") String accountTo, @RequestParam("amount") BigDecimal amount) {
+		log.info("Transfer amount from one to another account !!!!");
+
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
 
 }
